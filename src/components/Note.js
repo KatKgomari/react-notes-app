a5f4a6da97b6ecff7dc536e4d0c205af03deeabb
Katlego Kgomari <kgomari88@gmail.com>
2024-10-17 21:01:09 +0200

    created Note and NotesList components

---
+++ src/components/Note.js
@@ -0,0 +1,15 @@
+import {MdDeleteForever} from "react-icons/md"
+
+const Note = () =>{
+    return(
+        <div className="note">
+            <span>Hello, this is our first note!</span>
+            <div className="note footer">
+                <small>17/10/2024</small>
+                <MdDeleteForever className="delete-icon" size="1.3em"/>
+            </div>
+        </div>
+    )
+}
+
+export default Note;
