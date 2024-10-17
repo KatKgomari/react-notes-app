d1b6dcbe4c41e9f3a8935c70e21627540f5a9fbe
Katlego Kgomari <kgomari88@gmail.com>
2024-10-17 23:05:25 +0200

    added state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 
 const App = () => {
+  //list is rendered based on dynamic data. Using useState hook to hold our array of notes. Data wil be actively changed so it is put in state  
+  const [notes, setNotes] = useState([
+    {id: nanoid(), text: "This is my first note", date:"17/10/2024"},
+    {id: nanoid(), text: "This is my second note", date:"18/10/2024"},
+    {id: nanoid(), text: "This is my third note", date:"19/10/2024"},
+    {id: nanoid(), text: "This is my new note", date:"20/10/2024"},
+  ]);
   return <div className="container">
-    <NotesList/>
+    <NotesList notes={notes} />
   </div>
 }
 
